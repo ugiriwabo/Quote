d8969c3ae47cef6a9f0842d52e64c4db7bc16313
ugiriwabo <ugiraconsole@gmail.com>
2019-01-31 18:12:43 +0200

    collect error

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
     public showDescription:boolean;
-    constructor(public id:number, public name:string, public author:string,public description:string, public upvote:number, public downvote:number ,public Date:Date){
+    constructor(public id:number, public name:string, public author:string,public description:string, public upvote:number, public downvote:number ,public completeDate:Date){
         this.showDescription=false
 
     }
